8b7f140a897ab437538e47f5c7dcdb1b85140b19
Hussain00353 <x25177702@student.ncirl.ie>
2026-03-26 15:22:01 +0000

    add nginx security headers via ebextensions

--- jobofferapi/settings.py
+++ jobofferapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cb!1366in2f5pz!dt2^pg&sr)x7q9ldhcc^%5oky8n%l#1w^p$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
